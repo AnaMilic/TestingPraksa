8a1e751f6fd88680270229d94ccf3656eecda0f5
AnaMilic <76072963+AnaMilic@users.noreply.github.com>
2024-04-22 11:35:32 +0200

    Created visual test for Login page

--- tests/LoginTest.spec.js
+++ tests/LoginTest.spec.js
@@ -7,6 +7,8 @@ test("Valid login", async ({ page }) => {
   await page.getByPlaceholder("Username").fill("standard_user");
   await page.getByPlaceholder("Password").fill("secret_sauce");
   await page.getByRole("button", { name: "Login" }).click();
+  expect(await page.screenshot()).toMatchSnapshot("homePageScreenshot.png");
+  //await page.screenshot({ path: "screenshots/homePageScreenshot.png" });
   await itemTitle.first().waitFor();
   await expect(itemTitle.first()).toContainText("Sauce Labs Backpack");
 });
